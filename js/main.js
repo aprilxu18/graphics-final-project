b0ade20a08b6fe9141891fc8adfb157b6100f4c0
Kazen Gallman <kazar444@icloud.com>
2022-12-11 18:31:27 -0500

    particles somewhat working

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.mod
 import { TrackballControls } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/controls/TrackballControls.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/loaders/GLTFLoader.js';
+import { TWEEN } from 'https://unpkg.com/three@0.139.0/examples/jsm/libs/tween.module.min.js';
 
 // Load 3D Scene
 var scene = new THREE.Scene(); 
+var clock = new THREE.Clock();
 	
  // Load a Renderer
 var renderer = new THREE.WebGLRenderer({ alpha: false });
-renderer.setClearColor( 0xC5C5C3 );
+//renderer.setClearColor( 0xC5C5C3 );
+renderer.setClearColor( 0x000000 );
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -36,9 +39,9 @@ const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 // directionalLight.target.position.set(0, 0, 0);
 scene.add( directionalLight );
 
- // glTf 2.0 Loader
+// glTf 2.0 Loader
 var loader = new GLTFLoader();				
-	loader.load( '../media/farm-house.glb', function ( gltf ) {			
+loader.load( '../media/farm-house.glb', function ( gltf ) {			
 	gltf.scene.scale.set( 1 / 5, 1 / 5, 1 / 5 );			   
 	gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
         gltf.scene.position.y = 0;				    //Position (y = up+, down-)
@@ -46,13 +49,75 @@ var loader = new GLTFLoader();
     console.log(gltf)
     // camera = gltf.cameras[0];				    //Position (z = front +, back-)
 	scene.add( gltf.scene );
-	});	 
+});	 
 
-function animate() {
+
+const tloader = new THREE.TextureLoader();
+const spark = tloader.load("../images/spark.png")
+
+const material = new THREE.PointsMaterial({
+	size: 0.2,
+	color: 0xff3433,
+	map: spark,
+	transparent: true,
+	blending: THREE.AdditiveBlending
+})
+
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesCnt = 2000;
+
+const posArray = new Float32Array(particlesCnt * 3)
+
+for (let i = 0; i < particlesCnt * 3; i = i + 3) {
+	// x, y, z
+	posArray[i] = (Math.random() - 0.5) * 10
+	posArray[i + 1] = (Math.random() - 0.5) * 10
+	posArray[i + 2] = (Math.random() - 0.5) * 30
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
+
+const particlesMesh = new THREE.Points(particlesGeometry, material)
+
+scene.add(particlesMesh)
+
+console.log(particlesMesh)
+
+// let curAlphas = []
+// for (let i = 0; i < particlesCnt; i++) {
+// 	curAlphas = [...curAlphas, {"opacity" : 1}]
+// }
+// console.log(curAlphas)
+
+//TweenLite.to(particlesMesh.material, 1, {opacity: 0});
+// for (var i = 0; i < particlesMesh.length; i++) {
+// 	new TWEEN.Tween(curAlphas[i])
+// 		.to( { opacity:0.0 }, Math.random() * 10000)
+// 		.yoyo(true)
+// 		.repeat(Infinity)
+// 		.easing(TWEEN.Easing.Cubic.InOut)
+// 		.start();
+// }
+
+function animate(now) {
+
+	particlesMesh.rotation.x += 0.00002;
+
+	TWEEN.update();
+
+	// const opacityArray = new Float32Array(particlesCnt)
+	// for (let i = 0; i < particlesCnt; i++) {
+	// 	opacityArray[i] = curAlphas[i].opacity;
+	// }
+
+	// particlesGeometry.setAttribute('alpha', new THREE.BufferAttribute(opacityArray, 1));
+
+
 	render();
     controls.update();
 	requestAnimationFrame( animate );
-	}
+
+}
 
 function render() {
 	renderer.render( scene, camera );
